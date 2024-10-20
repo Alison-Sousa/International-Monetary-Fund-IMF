5bbf4689810e25066922007282856b2bc751b2f2
Alison-Sousa <116979938+Alison-Sousa@users.noreply.github.com>
2024-10-19 22:27:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,25 +25,21 @@ def get_indicators():
     indicators = {key: value['label'] for key, value in data['indicators'].items()}
     return indicators
 
-# Função para obter dados do FMI com depuração
+# Função para obter dados do FMI
 @st.cache_data
 def get_indicator_data(country_id, indicator_id, start_year, end_year):
     """Obter dados de um indicador específico para um país do FMI."""
-    try:
-        url = f"https://www.imf.org/external/datamapper/api/v1/data/{indicator_id}/{country_id}/{start_year}/{end_year}"
-        response = requests.get(url)
-        data = response.json()
+    url = f"https://www.imf.org/external/datamapper/api/v1/data/{indicator_id}/{country_id}/{start_year}/{end_year}"
+    response = requests.get(url)
+    data = response.json()
 
-        if not data or 'data' not in data:
-            st.error("Não foram encontrados dados para o país e indicador selecionados.")
-            return pd.DataFrame()
+    # Verificação se a resposta contém dados
+    if 'data' not in data or not data['data']:
+        return pd.DataFrame()  # Retorna DataFrame vazio se não houver dados
 
-        df = pd.DataFrame(data['data'])
-        df.rename(columns={'year': 'year', 'value': 'value'}, inplace=True)
-        return df[['year', 'value']]
-    except Exception as e:
-        st.error(f"Erro ao obter dados: {e}")
-        return pd.DataFrame()
+    df = pd.DataFrame(data['data'])
+    df.rename(columns={'year': 'year', 'value': 'value'}, inplace=True)
+    return df[['year', 'value']]
 
 # Sidebar para seleção
 with st.sidebar:
@@ -58,14 +54,16 @@ with st.sidebar:
     start_year = st.number_input("Ano de Início:", value=2000, min_value=1900, max_value=2024)
     end_year = st.number_input("Ano de Fim:", value=2024, min_value=1900, max_value=2024)
 
-    # Botão para obter dados
-    if st.button("Obter Dados"):
-        df = get_indicator_data(country_id, indicator_id, start_year, end_year)
-        if not df.empty:
-            st.session_state.df = df  # Armazena os dados em session_state para uso posterior
-            st.success("Dados obtidos com sucesso!")
-        else:
-            st.warning("Nenhum dado disponível para o intervalo de anos selecionado.")
+# Botão para obter dados
+if st.button("Obter Dados"):
+    df = get_indicator_data(country_id, indicator_id, start_year, end_year)
+    
+    # Verifica se o DataFrame está vazio
+    if df.empty:
+        st.error("Não foram encontrados dados para o país e indicador selecionados ou nenhum dado disponível para o intervalo de anos selecionado.")
+    else:
+        st.session_state.df = df  # Armazena os dados em session_state para uso posterior
+        st.success("Dados obtidos com sucesso!")
 
 # Exibição de gráficos e download
 if 'df' in st.session_state:
